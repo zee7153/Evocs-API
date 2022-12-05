5e8d4c52d3625d022ba8a0ed1a5a306397b54fbe
Zeeshan Naseer <zeeshan7153@gmail.com>
2022-12-05 11:36:09 +0500

    Subscribed Emails

--- routes/emailRoute.js
+++ routes/emailRoute.js
@@ -5,7 +5,7 @@ const db = require("../config/db")
 // Create route for fetch the email of subscribers
 
 router.get('/emails',(req,res)=>{
-    const sql='SELECT * FROM wp_mailpoet_subscribers;';
+    const sql='SELECT * FROM `wp_mailpoet_subscribers` where STATUS = "subscribed"';
     const query= db.query(sql,(err,result)=>{
         if(err){
             console.log(err)
